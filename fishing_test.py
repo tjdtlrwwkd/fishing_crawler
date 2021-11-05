452d578bf1beba10df9134492a7dfaa4945bddef
kss <tjdtlrwwkd@gmail.com>
2021-11-05 16:03:24 +0900

    fishing test update

--- fishing_test.py
+++ fishing_test.py
@@ -1,51 +1,15 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.by import By 
 
-import requests
 from bs4 import BeautifulSoup
+import requests
+import time
 import pandas
 import datetime
-
-"""
---------------------< 정보 입력 가이드 >--------------------
-
-    예약 검색 시작 날짜
-    "searching_start_date": Int (ex 20211109),
-    
-    예약 검색 종료 날짜
-    "searching_end_date": Int (ex 20211211),
-    
-    인원 수
-    "persons": Int (ex 1),
-    
-    예약자 이름
-    "name": String (ex "홍길동"),
-    
-    입금자 이름
-    "depositor": String (ex "홍길동"),
-    
-    핸드폰 번호
-    "cell_num": String (ex "010-XXXX-XXXX"),
-
-    두번째 페이지의 "예약 신청하기" 버튼을 자동으로 클릭하게 할 것인지 아닌지
-    "payment": Bool (ex True/False)
-
--------------------------------------------------------------
-"""
-
-info = {
-    "searching_start_date": 20211107,
-    "searching_end_date": 20211120,
-    "persons": 1,
-    "name": "홍길동",
-    "depositor": "홍길동",
-    "cell_num": "010-1234-5678",
-    "payment": False
-}
+import yaml
 
 # 예약한 화면을 캡쳐하는 시간(년, 월, 일, 시, 분, 초)으로 파일 이름 생성
 def create_time_name():
@@ -66,12 +30,11 @@ def getDay_c(year,month,day):
     return daylist[datetime.date(year,month,day).weekday()]
 
 # 예약 가능한 날짜 리턴하는 함수
-def search_reservation():
+def search_reservation(searching_start_date, searching_end_date):
     start_time = datetime.datetime.now()
 
     dates = date_range(str(info["searching_start_date"]), str(info["searching_end_date"]))
     possible_list = list()
-
     print("< 남당피싱 >")
     for date in dates:
         url = f'https://www.namdangfishing.com/_core/module/reservation_boat_v3/popup.step1.php?date={date}&PA_N_UID=563'
@@ -90,7 +53,7 @@ def search_reservation():
     
     return possible_list
 
-def auto_reservation(possible_list, date):
+def auto_reservation(possible_list, date, info):
     if date in possible_list:
         options = webdriver.ChromeOptions()
 
@@ -101,13 +64,15 @@ def auto_reservation(possible_list, date):
         
         options.add_argument("--headless")
         # 크롬드라이버가 설치된 경로로 크롬 웹 드라이버 객체 생성
-        driver = webdriver.Chrome('C:/kss/crawling/chromedriver', options=options)
+        # driver = webdriver.Chrome('C:/kss/crawling/chromedriver', options=options)
+        driver = webdriver.Chrome('crawling/chromedriver', options=options)
 
         try:
             start_time = datetime.datetime.now()
             # 남당피시 url Get
             driver.get(f'https://www.namdangfishing.com/_core/module/reservation_boat_v3/popup.step1.php?date={date}&PA_N_UID=563')
             
+            # 캡쳐할 화면의 크기 설정
             driver.set_window_size(800, 1150)
             # 체크박스 클릭
             driver.find_element(By.NAME, 'PS_N_UID').click()
@@ -161,14 +126,35 @@ def auto_reservation(possible_list, date):
         return False
 
 if __name__ == "__main__":
-    print("----------예약 가능한 날짜 리스트----------")
-    possible_list = search_reservation()
-    print("----------예약 가능 날짜 검색 종료----------")
-    print()
+    info = dict()
+
+    with open('C:\kss\crawling\info.yaml', 'r', encoding='UTF8') as f:
+        config = yaml.safe_load(f)
+        info["searching_start_date"] = config["searching_start_date"]
+        info["searching_end_date"] = config["searching_end_date"]
+        info["persons"] = config["persons"]
+        info["name"] = config["name"]
+        info["depositor"] = config["depositor"]
+        info["cell_num"] = config["cell_num"]
+        info["payment"] = config["payment"]
+
+        # print("< 낚시 예약 사이트 리스트 >")
+        # print("1. 남당피싱")
+        # print("2. 대천항 아인스월드")
+        # site_num = int(input())
+        # print("뒤로가기 : 0")
+
+        # if ord(input()) == 48:
+        #     # print("test")
+        #     continue
     while True: 
+        print("----------예약 가능한 날짜 리스트----------")
+        possible_list = search_reservation(info["searching_start_date"], info["searching_end_date"])
+        print("----------예약 가능 날짜 검색 종료----------")
         print("예약하실 날짜를 입력해주세요")
         date = input().replace('-', '')
-        if auto_reservation(possible_list, date):
+        if auto_reservation(possible_list, date, info):
             break
         else:
             continue
+    
